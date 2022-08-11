d4c8dfb73bb3219a3a7d74244c439cfcf47c9e4a
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2022-08-11 14:53:49 -0300

    animais route created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
+import { AnimaisModule } from './animais/animais.module';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: '', pathMatch:'full', redirectTo: 'home' },
   { path: 'home',
-  loadChildren: () => import('./home/home.module').then((m) => m.HomeModule) }
+  loadChildren: () => import('./home/home.module').then((m) => m.HomeModule) },
+  {
+    path: 'animais',
+    loadChildren: () =>
+    import('./animais/animais.module').then((m) => m.AnimaisModule),
+  }
 ];
 
 @NgModule({
