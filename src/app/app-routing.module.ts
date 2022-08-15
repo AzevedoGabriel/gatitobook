ff86a3f25dc058430eaa90a1a02bff3aaef7f187
AzevedoGabriel <gabriel.dantas.azevedo@ccc.ufcg.edu.br>
2022-08-15 09:04:52 -0300

    validando campos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,11 +5,12 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', pathMatch:'full', redirectTo: 'home' },
   { path: 'home',
-  loadChildren: () => import('./home/home.module').then((m) => m.HomeModule) },
+    loadChildren: () =>
+      import('./home/home.module').then((m) => m.HomeModule) },
   {
     path: 'animais',
     loadChildren: () =>
-    import('./animais/animais.module').then((m) => m.AnimaisModule),
+      import('./animais/animais.module').then((m) => m.AnimaisModule),
   }
 ];
 
